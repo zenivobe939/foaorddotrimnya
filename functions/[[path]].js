ff81ed6dfa471e97b1a72df4099afd9656b34b77
zenivobe939 <zenivobe939@gmail.com>
2025-02-28 18:33:40 +0300

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://aboba.co.ua'
+	const proxyUrl = 'https://sdfsdffsdsdfsdf.icu'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const headers = new Headers(request.headers)
